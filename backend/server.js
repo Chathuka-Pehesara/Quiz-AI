e6f0bc9a66dc1751647c1e15a754bcd0b747ef43
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-07-02 18:44:05 +0530

    added global JSON error handler

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,12 @@ app.use('/api/discussions', discussionRoutes);
 // Setup Socket.io battle logic
 require('./sockets/battle')(io);
 
+// Global JSON error handler
+app.use((err, req, res, next) => {
+  console.error('Unhandled server error:', err);
+  res.status(500).json({ message: err.message || 'Internal Server Error' });
+});
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
